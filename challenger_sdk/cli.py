fe4c47485fbd4f28b0203343cd9dd904bdc74b43
Fabian <fabian@sorqvist.nu>
2025-06-23 08:20:20 +0200

    add cli

---
+++ challenger_sdk/cli.py
@@ -0,0 +1,34 @@
+import typer
+import requests
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = typer.Typer()
+
+
+@app.command()
+def create_secret(name: str, value: str, bot_id: str | None = None):
+    url = os.getenv("CHALLENGER_URL")
+    if not url:
+        raise Exception(
+            "Challenger URL not present in your env file or your environment."
+        )
+    token = os.getenv("CHALLENGER_TOKEN")
+    if not token:
+        raise Exception(
+            "CHALLENGER_TOKEN not present in your env file or your environment."
+        )
+
+    headers = {"Authorization": f"Bearer {token}"}
+    data = {"name": name, "value": value}
+    response = requests.post(
+        f"{url}/api/bots/{bot_id}/secrets", json=data, headers=headers
+    )
+    if response.status_code != 201:
+        raise Exception(
+            f"Could not save secret: {response.status_code} {response.reason}"
+        )
+    print("Secret saved successfully!")
---
+++ tests/test_cli.py
@@ -0,0 +1,29 @@
+import os
+import pytest
+from challenger_sdk.cli import create_secret
+from unittest.mock import patch
+
+
+def test_missing_challenger_url():
+    with pytest.raises(Exception, match="Challenger URL not present"):
+        create_secret("secret", "value")
+
+
+def test_missing_challenger_token():
+    os.environ["CHALLENGER_URL"] = "http://example.com"
+    with pytest.raises(Exception, match="CHALLENGER_TOKEN not present"):
+        create_secret("secret", "value")
+    del os.environ["CHALLENGER_URL"]
+
+
+def test_create_secret():
+    os.environ["CHALLENGER_URL"] = "http://example.com"
+    os.environ["CHALLENGER_TOKEN"] = "example-token"
+    with patch("requests.post") as mock_request:
+        mock_request.return_value.status_code = 201
+        create_secret("secret", "value", "bot-id")
+        mock_request.assert_called_with(
+            "http://example.com/api/bots/bot-id/secrets",
+            json={"name": "secret", "value": "value"},
+            headers={"Authorization": "Bearer example-token"},
+        )
