ca1f9ea6f06de86364ab4851f9fbf562af39023c
Fabian <fabian@sorqvist.nu>
2025-09-16 09:00:49 +0200

    tools

---
+++ tests/test_manifest.py
@@ -0,0 +1,114 @@
+from fastapi.testclient import TestClient
+from pydantic import BaseModel
+
+from challenger_sdk.server import start_workflow_server
+from tests.test_server import get_component
+
+
+class Options(BaseModel):
+    notSecret: str
+
+
+class Secrets(BaseModel):
+    secret: str
+
+
+app = start_workflow_server("fns", "http://server.com", "key", Options, Secrets)
+client = TestClient(app)
+
+
+def test_manifest_data():
+    response = client.get(
+        "/manifest",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["subscription"] is None
+    component = data["actions"][0]
+    assert component["name"] == "store_favorite_food"
+    assert (
+        component["description"]
+        == "The user wants to tell you about their favorite food."
+    )
+    assert component["schema"]["call"]["type"] == "function"
+    call = component["schema"]["call"]["function"]
+    assert call["name"] == "store_favorite_food"
+    assert (
+        call["description"] == "The user wants to tell you about their favorite food."
+    )
+    assert call["parameters"]["type"] == "object"
+
+    assert call["parameters"]["properties"]["dish"]["type"] == "string"
+    assert call["parameters"]["required"] == ["dish"]
+
+
+def test_manifest_result_schema():
+    response = client.get(
+        "/manifest",
+    )
+    assert response.status_code == 200
+    data = response.json()
+
+    component = get_component(data["actions"], "say_hello")
+    assert component is not None
+    result = component["schema"]["result"]
+    assert result["properties"]["result"]["type"] == "string"
+    component = get_component(data["actions"], "say_hello_without_pydantic")
+    assert component is not None
+    result = component["schema"]["result"]
+    print(result)
+    assert result["properties"]["result"]["type"] == "string"
+
+
+def test_manifest_errors_schema():
+    response = client.get(
+        "/manifest",
+    )
+    assert response.status_code == 200
+    data = response.json()
+
+    component = get_component(data["actions"], "say_hello")
+    assert component is not None
+    errors = component["schema"]["errors"]
+    assert errors[0]["type"] == "object"
+    assert errors[0]["properties"]["message"]["type"] == "string"
+    assert errors[1]["type"] == "string"
+
+
+def test_manifest_exclude_ebbot():
+    response = client.get(
+        "/manifest",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    # Update user requires ebbot arguments and should not be included.
+    assert get_component(data["actions"], "retrieve_user") == None
+
+
+def test_manifest_component_result_null():
+    response = client.get(
+        "/manifest",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    # Update user requires ebbot arguments and should not be included.
+    component = get_component(data["actions"], "store_favorite_food")
+    assert component != None
+    assert component["schema"]["result"] == None
+
+
+def test_manifest_connection():
+    response = client.get(
+        "/manifest",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["connection"]["required"] == ["options", "secrets"]
+    assert (
+        data["connection"]["properties"]["options"]["properties"]["notSecret"]["type"]
+        == "string"
+    )
+    assert (
+        data["connection"]["properties"]["secrets"]["properties"]["secret"]["type"]
+        == "string"
+    )
